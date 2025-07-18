7718c43e1ff0f1091b5267d7c093157ce9f91e86
AuthorityWang <2453306868@qq.com>
2025-07-18 10:10:11 +0000

    20250718. Init

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+version = '0.0.1'
+
+setup(
+    name='partnext',
+    version=version,
+    description='A python lib for using PartNeXt dataset',
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/AuthorityWang/PartNeXt_lib',
+    author='AuthorityWang',
+    author_email='wangph1@shanghaitech.edu.cn',
+    packages=find_packages()
+)
